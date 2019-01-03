87515493869c1fa45130b0648d494edc5aa6f605
OmarTahoun <omaraiz489@gmail.com>
2019-01-03 14:52:03 +0300

    Add existing file

--- cell.py
+++ cell.py
@@ -1,15 +1,39 @@
-class cell(object):
-    """
-    docstring for cell.
+class Cell():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
 
-    1- each cell has x,y
-    2- each cell hass f(), h(), g()
-    3- neighbors for each cell
-    4- parent for each cell
+        self.neighbors = []
+        self.cameFrom = None
+
+    def get_neighbors(self, grid, rows, cols):
+        x = self.x
+        y = self.y
+
+        if x < rows-1:
+            self.neighbors.append(grid[x+1][y])
+            if y > 0:
+                self.neighbors.append(grid[x+1][y-1])
+            if y < cols-1:
+                self.neighbors.append(grid[x+1][y+1])
+
+        if x > 0:
+            self.neighbors.append(grid[x-1][y])
+            if y > 0:
+                self.neighbors.append(grid[x-1][y-1])
+            if y < cols-1:
+                self.neighbors.append(grid[x-1][y+1])
+
+        if y < cols-1:
+            self.neighbors.append(grid[x][y+1])
+
+        if y > 0:
+            self.neighbors.append(grid[x][y-1])
 
 
-    h(only four directions) = abs (current_cell.x – goal.x) + abs (current_cell.y – goal.y)  (right, left, up, down)
-    g() = cost from the begining to a cell
-    """
-    def __init__(self, arg):
-        self.arg = arg
+    def heuristic(self, goal):
+        distance = max(abs(self.x - goal.x), abs(self.y - goal.y))
+        return distance
--- search.py
+++ search.py
@@ -1,8 +1,69 @@
-# 1-  open set , closed set
-# 2- for each sucessor(neighbor) to a cell, calculate the score
-# 3- if neighbor is in open list and we reached it before with lower score :
-#     3.1- then skip this neighbor
-# 4- if neighbor is in closed list and we checked it before:
-#     4.1- if the earlier score is less than the current then skip
-#     4.2- else if we found a better route to this neighbor then add neighbor to open list
-#
+# Importing the cell object file
+from cell import *
+path = []
+
+# initializing the size of the grid to be 10 x 10
+rows = cols = 5
+# Creating the grid
+grid = [[0 for x in range(rows)] for y in range(cols)]
+
+for i in range(rows):
+    for j in range(cols):
+        grid[i][j] = Cell(i,j)
+
+for i in range(rows):
+    for j in range(cols):
+        grid[i][j].get_neighbors(grid, rows, cols)
+
+# Setting the start and end point
+start = grid[1][4]
+end = grid[3][1]
+
+# creating the set of open and closed cells
+open = []
+closed = []
+open.append(start)
+
+
+# Checking all the available or open cells
+while len(open)>0:
+    open.sort(key=lambda x: x.f)
+    q = open[0]
+    del open[0]
+
+
+    # Checking all the neighbors that we can visit from the current cell
+    for neighbor in q.neighbors:
+        print "checking neighbor: "
+        print neighbor.x,  neighbor.y
+
+        # Checking if that neighbor is the end point
+        if neighbor == end:
+            print("Done!")
+            neighbor.cameFrom = q
+            temp = neighbor
+            path.append(temp)
+            while(temp.cameFrom):
+                path.append(temp.cameFrom)
+                temp = temp.cameFrom
+            for cell in path:
+                print cell.x, cell.y
+            exit()
+
+
+        # Caclculating the score of this neighbor if visited from the current cell
+        temp_g = q.g+1
+        temp_h = neighbor.heuristic(end)
+        temp_f = temp_g + temp_h
+
+
+        # If this neighbor is already in the open set
+        if neighbor not in closed:
+            if neighbor not in open:
+                neighbor.cameFrom = q
+                open.append(neighbor)
+            elif temp_f > neighbor.f:
+                continue
+
+
+    closed.append(q)
